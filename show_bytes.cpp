cdec3568a655e5f20b0af51df619a741e901ee89
cosmore <icosmore@foxmail.com>
2021-08-22 22:09:00 +0800

    add submodule tau for c unittest

--- show_bytes.cpp
+++ show_bytes.cpp
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
-typedef unsigned char* byte_pointer;
-typedef void* pointer_type;
+typedef unsigned char *byte_pointer;
+typedef void *pointer_type;
 
 void show_bytes(byte_pointer pointer, unsigned size)
 {
-    for (unsigned i = 0; i < size; i++) 
+    for (unsigned i = 0; i < size; i++)
     {
         printf("%02x ", pointer[i]);
     }
@@ -13,39 +13,58 @@ void show_bytes(byte_pointer pointer, unsigned size)
 }
 
 template <typename T>
-const char* type_name(const T& name);
+const char *type_name(const T &name);
 
-template<> const char* type_name(const int& name)
+template <>
+const char *type_name(const short &name)
+{
+    return "short";
+}
+
+template <>
+const char *type_name(const unsigned short &name)
+{
+    return "unsigned short";
+}
+
+template <>
+const char *type_name(const int &name)
 {
     return "int";
 }
 
-template<> const char* type_name(const unsigned int& name)
+template <>
+const char *type_name(const unsigned int &name)
 {
     return "unsigned int";
 }
 
-template<> const char* type_name(const long& name)
+template <>
+const char *type_name(const long &name)
 {
     return "long";
 }
 
-template<> const char* type_name(const unsigned long& name)
+template <>
+const char *type_name(const unsigned long &name)
 {
     return "unsigned long";
 }
 
-template<> const char* type_name(const float& name)
+template <>
+const char *type_name(const float &name)
 {
     return "float";
 }
 
-template<> const char* type_name(const double& name)
+template <>
+const char *type_name(const double &name)
 {
     return "double";
 }
 
-template<> const char* type_name(const pointer_type& name)
+template <>
+const char *type_name(const pointer_type &name)
 {
     return "pointer";
 }
@@ -57,9 +76,19 @@ void show_value_bytes(T value)
     show_bytes((byte_pointer)&value, sizeof(T));
 }
 
-int main()
+void test_signed_ints()
 {
     int varInt = 0x12345678;
+    show_value_bytes((short)-varInt);
+    show_value_bytes((unsigned short)-varInt);
+}
+
+void test_number_type()
+{
+    int varInt = 0x12345678;
+    show_value_bytes((short)varInt);
+    show_value_bytes((unsigned short)varInt);
+
     show_value_bytes(varInt);
     show_value_bytes((unsigned)varInt);
 
@@ -70,5 +99,11 @@ int main()
     show_value_bytes((double)varInt);
 
     show_value_bytes((pointer_type)&varInt);
+}
+
+int main()
+{
+    test_number_type();
+    test_signed_ints();
     return 0;
 }
